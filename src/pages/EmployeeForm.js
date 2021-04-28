91c12ec9415f88eaba5483c2ebb24d6ce4865d35
vishakha2907 <78352835+vishakha2907@users.noreply.github.com>
2021-04-28 20:57:31 +0530

    Create EmployeeForm.js

---
+++ src/pages/EmployeeForm.js
@@ -0,0 +1,141 @@
+import React, { useState, useEffect } from 'react'
+import { Grid, } from '@material-ui/core';
+import Controls from "../../components/controls/Controls";
+import { useForm, Form } from '../../components/useForm';
+import * as employeeService from "../../services/employeeService";
+
+
+
+
+const initialFValues = {
+    id: 0,
+    fullName: '',
+    username:'',
+    password:'',
+    address:'',
+    mobile: '',
+    email: '', 
+    departmentId: '',
+  
+}
+
+export default function EmployeeForm(props) {
+    const { addOrEdit } = props
+
+    const validate = (fieldValues = values) => {
+        let temp = { ...errors }
+        if ('fullName' in fieldValues)
+            temp.fullName = fieldValues.fullName ? "" : "This field is required."
+        if ('username' in fieldValues)
+            temp.username = fieldValues.username ? "" : "This field is required."
+        if ('password' in fieldValues)
+            temp.password = fieldValues.password ? "" : "This field is required."
+        if ('address' in fieldValues)
+            temp.address = fieldValues.address ? "" : "This field is required."
+        if ('email' in fieldValues)
+            temp.email = (/$^|.+@.+..+/).test(fieldValues.email) ? "" : "Email is not valid."
+        if ('mobile' in fieldValues)
+            temp.mobile = fieldValues.mobile.length > 9 ? "" : "Minimum 10 numbers required."
+        if ('departmentId' in fieldValues)
+            temp.departmentId = fieldValues.departmentId.length != 0 ? "" : "This field is required."
+        setErrors({
+            ...temp
+        })
+
+        if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+    }
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    } = useForm(initialFValues, true, validate);
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        if (validate()) {
+            addOrEdit(values, resetForm);
+        }
+    }
+
+   
+    return (
+        <Form onSubmit={handleSubmit}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <Controls.Input
+                        name="fullName"
+                        label="Full Name"
+                        value={values.fullName}
+                        onChange={handleInputChange}
+                        error={errors.fullName}
+                    />
+                    <Controls.Input
+                        name="username"
+                        label="Username"
+                        value={values.username}
+                        onChange={handleInputChange}
+                        error={errors.username}
+                    />
+                    <Controls.Input
+                        name="password"
+                        label="Password"
+                        type="password"
+                        value={values.password}
+                        onChange={handleInputChange}
+                        error={errors.password} 
+                    />
+                    <Controls.Input
+                        name="address"
+                        label="Address"
+                        value={values.address}
+                        onChange={handleInputChange}
+                        error={errors.address}
+                    />
+                   
+                   
+                   
+                </Grid>
+                <Grid item xs={6}>
+                <Controls.Input
+                        label="Mobile"
+                        name="mobile"
+                        value={values.mobile}
+                        onChange={handleInputChange}
+                        error={errors.mobile}
+                    /> 
+                     <Controls.Input
+                        label="Email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleInputChange}
+                        error={errors.email}
+                    />
+                    <Controls.Select
+                        name="departmentId"
+                        label="Location Preference"
+                        value={values.departmentId}
+                        onChange={handleInputChange}
+                        options={employeeService.getDepartmentCollection()}
+                        error={errors.departmentId}
+                    />
+                    
+
+                    <div>
+                        <Controls.Button
+                            type="submit"
+                            text="Submit" />
+                        <Controls.Button
+                            text="Reset"
+                            color="default"
+                            onClick={resetForm} />
+                    </div>
+                </Grid>
+            </Grid>
+        </Form>
+    )
+}
